34918cde13fd28310a40411f5ace85763d94686d
Divya Chaudhary <108574254+D1vyA21011@users.noreply.github.com>
2023-08-16 07:52:59 -0400

    Create Macchabger.sh

---
+++ Macchabger.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+clear
+echo "################# MAC Changer ################"
+echo
+echo "*********Developed by D1vy@ ***********"
+echo 
+echo "1) eth0"
+echo "2) Wlan0"
+echo
+echo "++++++++++++++++++++++++++++++++++++++++++++++"
+echo
+
+echo -en "What Interface MAC ID You Want To change: "
+read id
+
+
+if [[ $id = 1 ]]; then
+start=$( date +%s )
+echo "Changing MAC ID.... Wait..."
+sudo ifconfig eth0 down
+
+sudo macchanger -r eth0
+
+sudo ifconfig eth0 up
+end=$( date +%s )
+timetaken=$(( $start - $end ))
+echo 
+echo
+echo
+echo "eth0 MAC ID is Successfully Changed in $timetaken Seconds"
+
+
+elif [[ $id = 2 ]]; then
+
+start=$( date +%s )
+echo "Changing MAC ID.... Wait..."
+sudo ifconfig wlan0 down
+
+sudo macchanger -r wlan0
+
+sudo ifconfig wlan0 up
+end=$( date +%s )
+timetaken=$(( $start - $end ))
+echo 
+echo 
+echo "Wlan0  MAC ID is Successfully Changed in $timetaken Seconds"
+	if [[ $? = 1 ]]; then
+		echo > /dev/null 2>&1
+
+	fi
+fi
